d231462e002f2d1beb9eb05a90793de587ba8be3
abdushukurova <abdushukurova.km@gmail.com>
2019-04-28 12:33:48 +0300

    UI works

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    [SerializeField] AudioSource m_Background;
+    [SerializeField] AudioSource m_Hit;
+    [SerializeField] float m_HitFrequency;
+
+    float m_LastHit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Background.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - m_LastHit > m_HitFrequency)
+        {
+            m_LastHit = Time.time;
+            m_Hit.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/Editor/AudioClipUtility.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public static class AudioClipUtility
+{
+    [MenuItem("Assets/Convert to clip")]
+    public static void ConvertToClip()
+    {
+        foreach (var obj in Selection.objects)
+        {
+            AudioClip clip = obj as AudioClip;
+            if (clip != null)
+            {
+                for (int i = 0; i < clip.channels; i++)
+                {
+                    AnimationClip animation = new AnimationClip();
+                    List<AnimationEvent> events = new List<AnimationEvent>();
+
+
+                    float[] samples = new float[clip.samples];
+                    clip.GetData(samples, i);
+
+                    bool boom = false;
+                    for (int j = 0; j < samples.Length; j++)
+                    {
+                        float s = samples[j];
+
+                        if (s > .92f && !boom)
+                        {
+                            boom = true;
+
+                            AnimationEvent ev = new AnimationEvent();
+                            ev.stringParameter = "boom";
+                            ev.time = clip.length * (j / (float)samples.Length);
+
+                            events.Add(ev); 
+                        }
+                        else
+                            boom = false;
+                    }
+
+                    AnimationUtility.SetAnimationEvents(animation, events.ToArray());
+
+                    AssetDatabase.CreateAsset(animation, AssetDatabase.GenerateUniqueAssetPath("Assets/animation.anim")); 
+                }
+            }
+
+        }
+    }
+}
