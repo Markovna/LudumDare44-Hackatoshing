301d5409cb89f732560f4f8bc866e192a30b33d4
abdushukurova <abdushukurova.km@gmail.com>
2019-04-28 16:08:55 +0300

    game v1

---
+++ Assets/Scripts/AudioSourcePool.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+public class AudioSourcePool : MonoBehaviour
+{
+    List<AudioSource> m_Pool;
+    int m_InitCount = 10;
+
+    private void Awake()
+    {
+        m_Pool = GetComponents<AudioSource>().ToList();
+        if (m_Pool.Count < m_InitCount)
+        {
+            int count = m_InitCount - m_Pool.Count;
+            for (int i = 0; i < count; i++ )
+            {
+                m_Pool.Add(gameObject.AddComponent<AudioSource>());
+            }
+        }
+
+        foreach (var s in m_Pool)
+            InitSoure(s);
+    }
+
+    public AudioSource GetAudioSource(float _Time)
+    {
+        AudioSource source = GetAudioSource();
+        StartCoroutine(Delay(_Time, () => DestroySource(source)));
+        return source;
+    }
+
+    public AudioSource GetAudioSource()
+    {
+        AudioSource source;
+        if (m_Pool.Count < 1)
+        {
+            source = gameObject.AddComponent<AudioSource>();
+            InitSoure(source);
+            return source;
+        }
+
+        source = m_Pool[0];
+        InitSoure(source);
+        m_Pool.RemoveAt(0);
+        return source;
+    }
+
+    void InitSoure(AudioSource _Source)
+    {
+        _Source.playOnAwake = false;
+    }
+
+    public void DestroySource(AudioSource _Source)
+    {
+        m_Pool.Add(_Source);
+    }
+
+    IEnumerator Delay(float _Delay, Action _Callback)
+    {
+        yield return new WaitForSeconds(_Delay);
+
+        if (_Callback != null)
+            _Callback();
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,20 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class GameController : MonoBehaviour
 {
-    [SerializeField] AudioSource m_AudioSource;
-    [SerializeField] Text m_Text;
+    [SerializeField] AudioController m_AudioController;
+    [SerializeField] TimerController m_TimerController;
+    [SerializeField] GameObject m_UI;
+    [SerializeField] float m_Seconds = 30f;
+    [SerializeField] float m_RealSeconds = 15f;
 
-    // Start is called before the first frame update
-    void Start()
+    float m_StartTime = -1f;
+    float m_Scale;
+
+    int m_LastSeconds = -1;
+
+    public void StartGame()
     {
+        m_StartTime = Time.time;
+        m_Scale = m_Seconds / m_RealSeconds;
+
+        m_UI.SetActive(true);
+
+        m_AudioController.PlayBackground();
     }
 
-    // Update is called once per frame
+    public void StopGame()
+    {
+        m_StartTime = -1;
+
+        m_UI.SetActive(false);
+
+        m_AudioController.StopBackground();
+    }
+
     void Update()
     {
-        
+        if (m_StartTime < 0)
+            return;
+
+        float current = Time.time - m_StartTime;
+        TimeSpan time = TimeSpan.FromSeconds(current* m_Scale);
+
+        int sec = time.Seconds;
+        if (sec > m_LastSeconds)
+            m_AudioController.PlayHit();
+
+        m_LastSeconds = sec;
+
+        m_TimerController.SetTime(time);
     }
 }
